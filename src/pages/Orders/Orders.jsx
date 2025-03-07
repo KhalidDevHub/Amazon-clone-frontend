6b7b3e44de62746876c742c25da0bea55900269e
Sara Ali <kalestars@gmail.com>
2025-03-07 13:49:05 +0300

    ordes UI pages

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
-import Layout from "../../Components/Layout/Layout"
+import React, { useContext, useEffect, useState } from "react";
+import classes from "./Orders.module.css";
+import Layout from "../../Components/Layout/Layout";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
+import { db } from "../../utility/firebase";
+import ProductCard from "../../Components/Product/ProductCard";
+function Orders() {
+  const [{ user }, dispatch] = useContext(DataContext);
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    if (user) {
+      db.collection("user")
+        .doc(user.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) => {
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          );
+        });
+    } else {
+      setOrders([]);
+    }
+  });
 
-
-const Orders = () => {
+  console.log(orders);
   return (
-    <Layout>Orders</Layout>
-  )
+    <Layout>
+      <section className={classes.container}>
+        <div className={classes.orders_Container}>
+          <h2>Your Orders</h2>
+          {/* ordered items */}
+          {orders?.length == 0 && (
+            <div style={{ padding: "20px" }}>you don't have orders yet.</div>
+          )}
+          <div>
+            {orders?.map((eachOrder, i) => {
+              return (
+                <div key={i}>
+                  <hr />
+                  <p>Order ID: {eachOrder.id}</p>
+                  {eachOrder?.data?.basket?.map((order) => {
+                    return (
+                      <ProductCard product={order} key={order.id} flex={true} />
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
-export default Orders
+export default Orders;
