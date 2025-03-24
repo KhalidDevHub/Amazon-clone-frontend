8bfcb4c7f3ccee8eb25e099f656268d5fc5a8504
Khalid.A <kalestars@gmail.com>
2025-03-24 06:01:10 +0300

    update

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 export const axiosInstance = axios.create({
   //for local server firebase function
-  baseURL: "http://127.0.0.1:5001/clone-c9bf9/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-c9bf9/us-central1/api",
 
-  // deployed version of amazon server in render.com
-  // baseURL: "https://amazon-backend-deploy-dotb.onrender.com/",
+  // deployed version of amazon server in render.com 
+  baseURL: "https://amazone-api-backend.onrender.com",
 });
